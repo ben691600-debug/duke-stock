adaf449a66de27e5f2ea2241bba97bdd3d1f0260
ben691600-debug <ben691600@gmail.com>
2026-06-18 16:05:18 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Duke Stock Manager - Service Worker v8 - NETWORK FIRST + activation automatique des mises à jour
-const CACHE_NAME = 'duke-stock-v13';
+const CACHE_NAME = 'duke-stock-v14';
 
 // Installation : on active IMMÉDIATEMENT le nouveau SW dès qu'il est installé,
 // pour que les mises à jour (correctifs de sync, etc.) s'appliquent automatiquement
