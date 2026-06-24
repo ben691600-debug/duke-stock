6d51b920f4be3d7ee024c2edbd53b812ab8a4fcc
ben691600-debug <ben691600@gmail.com>
2026-06-24 07:16:49 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 // Duke Stock Manager - Service Worker v8 - CACHE FIRST avec détection de mise à jour
-const CACHE_NAME = 'duke-stock-v100';
+const CACHE_NAME = 'duke-stock-v101';
 const FILES_TO_CACHE = ['/', '/index.html', '/manifest.json', '/logo.png'];
 
 self.addEventListener('install', event => {
