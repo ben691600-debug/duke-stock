f4456c6b59771cda9ca6cdfded007747aee6326f
ben691600-debug <ben691600@gmail.com>
2026-07-03 19:29:38 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'duke-stock-v128';
+const CACHE_NAME = 'duke-stock-v129';
 // index.html n'est JAMAIS mis en cache - toujours servi depuis le réseau
 // Seuls les assets statiques qui ne changent pas sont cachés
 const STATIC_ASSETS = [
