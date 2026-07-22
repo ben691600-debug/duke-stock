af220f1df3be8b0c7a77d71f303dba5c467ea3da
ben691600-debug <ben691600@gmail.com>
2026-07-22 12:08:09 +0300

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'duke-stock-v191';
+const CACHE_NAME = 'duke-stock-v192';
 // index.html n'est JAMAIS mis en cache - toujours servi depuis le réseau
 // Seuls les assets statiques qui ne changent pas sont cachés
 const STATIC_ASSETS = [
@@ -46,4 +46,4 @@ self.addEventListener('fetch', event => {
   );
 });
 
-// Cache busted: 20260722120000
+// Cache busted: 20260722121500
